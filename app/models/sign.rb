ef0678f61859eb28652489c2fae9f79162cbe4fd
Latricia <tricianickelberry@gmail.com>
2020-06-01 11:29:11 -0700

    added associations to models

--- app/models/sign.rb
+++ app/models/sign.rb
@@ -1,2 +1,4 @@
 class Sign < ApplicationRecord
+    has_many :users
+    has_many :user_comments through :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
     belongs_to :sign
-    
+    has_many :signs through :comments
 end
